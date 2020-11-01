03f76984762323fa37e15abb279f5301fe4ee7c4
btjmga <bartlomiej.ryszard.magiera@gmail.com>
2020-11-02 00:24:51 +0100

    rename variables

--- src/twitch_getallvods.py
+++ src/twitch_getallvods.py
@@ -14,21 +14,21 @@ def get_oauth(g_oa_client_id, g_oa_client_secret):  # This method gets the auth 
         "scope": "clips:edit",
     }
     url_get_oauth = "https://id.twitch.tv/oauth2/token"
-    response_get_oauth = session.post(url_get_oauth, params=params_get_oauth)
-    response_get_oauth_json = response_get_oauth.json()
-    return response_get_oauth_json["access_token"]
+    g_oa_response = session.post(url_get_oauth, params=params_get_oauth)
+    g_oa_reponse_json = g_oa_response.json()
+    return g_oa_reponse_json["access_token"]
 
 
 def get_userid(
     g_us_username, g_us_headers
 ):  # This method gets twitch user id using username
-    params_get_userid = {"login": g_us_username}
-    url_get_userid = "https://api.twitch.tv/helix/users"
-    response_get_userid = session.get(
-        url_get_userid, params=params_get_userid, headers=g_us_headers
+    g_us_params = {"login": g_us_username}
+    g_us_userid = "https://api.twitch.tv/helix/users"
+    g_us_response = session.get(
+        g_us_userid, params=g_us_params, headers=g_us_headers
     )
-    reponse_get_userid_json = response_get_userid.json()
-    return reponse_get_userid_json["data"][0]["id"]
+    g_us_response_json = g_us_response.json()
+    return g_us_response_json["data"][0]["id"]
 
 
 def populate_lists(
@@ -37,13 +37,13 @@ def populate_lists(
     p_li_url_list,
 ):  # this method comes from previous get_videos method
     for i in range(0, len(p_li_json["data"])):
-        videos_desc_from_json = (
+        p_li_video_desc = (
             p_li_json["data"][i]["title"] + " " + p_li_json["data"][i]["created_at"]
         )
-        videos_desc_from_json_no_special_chars = re.sub(
-            "[^A-Za-z0-9]+", " ", videos_desc_from_json
+        p_li_video_desc_parse = re.sub(
+            "[^A-Za-z0-9]+", " ", p_li_video_desc
         )
-        p_li_title_list.append(videos_desc_from_json_no_special_chars)
+        p_li_title_list.append(p_li_video_desc_parse)
         p_li_url_list.append(p_li_json["data"][i]["url"])
 
 
@@ -54,30 +54,30 @@ def get_videos(
     g_vi_url_list,
     cursor=None,
 ):  # This method gets all vod urls and creates 2 lists - vod names and urls
-    params_get_videos = {("user_id", g_vi_userid)}
+    g_vi_params = {("user_id", g_vi_userid)}
     if cursor is not None:
-        params_get_videos = list(params_get_videos) + list({("after", cursor)})
-    url_get_videos = "https://api.twitch.tv/helix/videos"
-    response_get_videos = session.get(
-        url_get_videos, params=params_get_videos, headers=g_vi_headers
+        g_vi_params = list(g_vi_params) + list({("after", cursor)})
+    g_vi_url = "https://api.twitch.tv/helix/videos"
+    g_vi_response = session.get(
+        g_vi_url, params=g_vi_params, headers=g_vi_headers
     )
-    reponse_get_videos_json = response_get_videos.json()
-    populate_lists(reponse_get_videos_json, g_vi_title_list, g_vi_url_list)
-    if "cursor" in reponse_get_videos_json["pagination"]:
+    g_vi_response_json = g_vi_response.json()
+    populate_lists(g_vi_response_json, g_vi_title_list, g_vi_url_list)
+    if "cursor" in g_vi_response_json["pagination"]:
         get_videos(
             g_vi_userid,
             g_vi_headers,
             g_vi_title_list,
             g_vi_url_list,
-            reponse_get_videos_json["pagination"]["cursor"],
+            g_vi_response_json["pagination"]["cursor"],
         )
 
 
 def download_videos(
     d_vi_description, d_vi_url
 ):  # This method lets you download all vods 1 by 1
-    user_input = input("Do you want to download all VODs? (y/n): ")
-    if user_input == "y":
+    d_vi_user_input = input("Do you want to download all VODs? (y/n): ")
+    if d_vi_user_input == "y":
         for i in range(0, len(d_vi_url)):
             stream = streamlink.streams(d_vi_url[i])["best"].url
             download = subprocess.Popen(
@@ -92,7 +92,7 @@ def download_videos(
                 ]
             )
             download.wait()
-    elif user_input == "n":
+    elif d_vi_user_input == "n":
         return 0
     else:
         print("Wrong choice, start again")
